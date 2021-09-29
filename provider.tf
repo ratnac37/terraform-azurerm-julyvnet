b6cdca61010b7007bca5de0caccdf867cfbb06b0
prasad <prasad@gmail.com>
2021-09-29 07:27:16 +0530

    added files

---
+++ provider.tf
@@ -0,0 +1,9 @@
+
+provider "azurerm" {
+  features {}
+  version = "=2.46.0"
+  subscription_id = "dbd68380-ce63-4235-8632-ddf2852afc17"
+  client_id       = "fe78821d-c02d-449b-a565-fb84a04b63b7"
+  client_secret   = "M-l7Q~~Ra6EN.5K.ZkZyeqO.dStuqCK2Droke"
+  tenant_id       = "5af85c72-b5b8-42e2-b4b8-325ad97c85cd"
+}
